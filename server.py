bbe02bd463f53faf4c8293fb883d86e5edb326df
celskiryan-wq <celski.ryan@gmail.com>
2026-07-08 21:13:24 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,16 @@ PORT = int(os.environ.get("PORT", 8080))
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory="frontend", **kwargs)
+    
+    def do_GET(self):
+        # Serve index.html for root
+        if self.path == "/":
+            self.path = "/index.html"
+        # Serve admin.html for /admin
+        elif self.path == "/admin":
+            self.path = "/admin.html"
+        return super().do_GET()
+    
     def log_message(self, format, *args):
         pass
 
